36967f04a9fce80d954fa586833691238fdbb9de
Anna Konstantynova <konstanna.dev@gmail.com>
2024-06-18 19:51:42 +0200

    Add MoviesController and index page

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
     #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->render();
+        $movies = ["Pride and Prejudice", "Persuasion", "Emma", "Sense and Sensibility"];
+
+        return $this->render('index.html.twig', array(
+            'movies' => $movies
+        ));
     }
 }
